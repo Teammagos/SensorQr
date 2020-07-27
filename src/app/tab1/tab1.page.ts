eeb3dabbce25045e70a39fa8bd0c70177f8fb6e1
kevin <kevinchacha29@yahoo.com>
2020-07-26 21:16:17 -0500

    firts commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from '../services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(private barcodeScanner: BarcodeScanner, private datalocal:DataLocalService) {}
+  ionViewDidEnter(){
+    console.log("ionViewDidEnter");
+    this.scan();
+  }
+  ionViewDidLeave(){
+    console.log("ionViewDidleave");
+  }
+  scan(){
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      if(!barcodeData.cancelled){
+        this.datalocal.guardarRegistro(barcodeData.format,barcodeData.text);
+      }
+     }).catch(err => {
+         console.log('Error', err);
+         this.datalocal.guardarRegistro('html','http://www.ups.edu.ec');
+     });
+  }
 }
